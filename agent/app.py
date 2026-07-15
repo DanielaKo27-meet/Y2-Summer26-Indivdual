2b668f724b53ac024b395d1d06e743521b76ac69
DanielaKo27-meet <daniela.koretsky@gmail.com>
2026-07-15 18:40:14 +0300

    the_reel_lab_2
    I am sorry this is the correct code for lab 2

--- agent/app.py
+++ agent/app.py
@@ -8,7 +8,23 @@ client = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
 
 def run_chat():
     print('You: (type exit to quit)')
-    system_message = "Your name is Afrodita. You are a rude assistant who does nothing and doesn't help."
+    system_message = """
+You are Afrodita, a friendly but pasemistic person.
+
+Your job is to help me with my questions and concerns.
+
+Rules:
+- Always see if i have a mistake and halp me
+- Always say the harsh truth
+- Never lie or make a mistake or a problem look smaller than it is
+
+Response format:
+- your responses are on point without to much intro
+- End with one follow-up question.
+"""
+
+
+
     history = []
 
     while True:
@@ -28,7 +44,6 @@ def run_chat():
         reply = response.content[0].text
      #   print(response)
         print(f'Claude: {reply}')
-      # print(len(history)//2)
         history.append({'role': 'assistant', 'content': reply})
 
 run_chat()
@@ -46,9 +61,10 @@ run_chat()
 # 2) if i change max_tokens to 50 clouds cuts its answers and i dint see the end of the messege.
 #    if i change temperature to 0 i dont really see a difference even though i know it is supposed to make answers more random
 #    tempruture controles how random the answer will be
-# 3) 
+# 3) 5, it has no mamory so it needs all the info that we gave him befor and what it answers were 
 #--------Reflection_2----------#
 # 1) when i go to the gym i pay for a trainer and it feeles like an ok amount of money but when i think about it i understand that i will spend most of my money on it.
-# 2) history.append({'role': 'user', 'content': user_input}) — what does the AI now receive, and what happens to input_tokens?
-#    history.append({'role': 'assistant', 'content': reply}) — what does the AI forget, and how does the token count grow differently?
-#    print('History so far:', history) — you lose visibility... but does the AI behave any differently? (Difference between debug output and logic.)
+# 2) history.append({'role': 'user', 'content': user_input}) — what does the AI now receive, and what happens to input_tokens? This line adds the user input to the history. So now, the AI will receive only the current user input without any previous chat context, and input_tokens will be low because the past inputs won't count.
+#    history.append({'role': 'assistant', 'content': reply}) — what does the AI forget, and how does the token count grow differently?  The AI will forget its own past replies, but it will still remember the user's questions. The token count will continue to grow, but more slowly, because only the user's side of the conversation is being saved.
+#    print('History so far:', history) — you lose visibility... but does the AI behave any differently? (Difference between debug output and logic.)  it wont change anithing becaus it just print (shows) you what hepends behind the scenes
+# 3) i didnt have any changes when i did the first expariment i thought that i changed the wrong thing but aperently i had to save again all of the code for it to work i am so used to to idol that if i have a probrem i think it is the code and not the saving or something similar 
